8861babf6a7b780c02defd28b51cd49dacaf44e7
syzhang <hefeizsy@163.com>
2019-01-05 00:51:51 -0500

    Create reformat_json.py

---
+++ reformat_json.py
@@ -0,0 +1,16 @@
+import json
+import numpy as np
+
+if __name__ == '__main__':
+
+    for split in ['train','val_1','val_2']:
+        anno_list = json.load(open('data/{}.json'.format(split)))
+        json_path = 'data/new_{}.json'.format(split)
+
+        data = []
+        for vid, info in anno_list.items():
+            for (start, end), sentence in zip(info['timestamps'],info['sentences']):
+                row = {'video':vid, 'description': sentence, 'times':[[float(start), float(end)]], 'duration':info['duration']}
+            data.append(row)
+        with open(json_path, 'w') as json_file:
+            json.dump(data, json_file)
